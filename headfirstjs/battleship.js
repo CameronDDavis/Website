08b1596bd939b6456b241cc2e2ccb0d4a2334711
CameronDDavis <cameron.dale.davis@gmail.com>
2019-11-26 08:23:17 -0500

    added random places for my battleship

--- headfirstjs/battleship.js
+++ headfirstjs/battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -25,6 +26,6 @@ while (isSunk == false) {
 		}
 	}
 }
-var stats = "You took " + guesses + " guesses to sink the battle ship, " + "which means your shooting accuracy was " + (3/guesses);
+var stats = "You took " + guesses + " guesses to sink the battle ship, " + "which means your shooting accuracy was " + (3/guesses * 100) + "%";
 
 alert(stats);
